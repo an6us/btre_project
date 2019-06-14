e6f0892ecd78064fde937498d276ef92db569cfa
an6us <aafriend1@gmail.com>
2019-06-14 22:22:36 +0800

    added local setting import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shoecanada@gmail.com'
 EMAIL_HOST_PASSWORD = 'angus999'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
