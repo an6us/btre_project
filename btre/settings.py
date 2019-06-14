b7d843b1e2143eeae91ec7f777baa43cb9a8f561
an6us <aafriend1@gmail.com>
2019-06-14 22:58:08 +0800

    delete email settings in settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -143,11 +143,7 @@ MESSAGE_TAGS = {
 }
 
 #email config 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shoecanada@gmail.com'
-EMAIL_HOST_PASSWORD = 'angus999'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
